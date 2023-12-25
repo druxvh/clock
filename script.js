edcb72e6f1b911cb212d6362e7bce344a1eff432
dm <heydruavh@gmail.com>
2023-12-26 00:25:07 +0530

    improved timeLoad func

--- script.js
+++ script.js
@@ -7,25 +7,19 @@ function timeLoad() {
   let seconds = newDate.getSeconds();
   let time, session;
 
-  switch (true) {
-    case hours < 10:
-      hours = "0" + hours;
-      break;
-    case minutes < 10:
-      minutes = "0" + minutes;
-      break;
-    case seconds < 10:
-      seconds = "0" + seconds;
-      break;
+  if (hours == 0) {
+    hours = 12;
   }
-  if(hours > 12){
-    hours = "0" + (hours - 12)
-  }
-  if (hours <= 12) {
+  if (hours > 12) {
+    hours = hours - 12;
     session = "PM";
   } else {
     session = "AM";
   }
+
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
 
   time = `${hours}:${minutes}:${seconds} ${session}`;
   (display.textContent = time), setTimeout(timeLoad, 1000);
