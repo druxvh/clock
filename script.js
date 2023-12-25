e1f73e1d35419e05465a35cd7058dd3997618d81
dm <heydruavh@gmail.com>
2023-12-25 17:54:01 +0530

    Improved timeLoad Function

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
-let display = document.querySelector("span")
-
+let display = document.querySelector("span");
 
 function timeLoad() {
-const newDate = new Date()
-let hours = newDate.getHours()
-let minutes = newDate.getMinutes()
-let seconds = newDate.getSeconds()
-let time = "AM";
-let session;
+  const newDate = new Date();
+  let hours = newDate.getHours();
+  let minutes = newDate.getMinutes();
+  let seconds = newDate.getSeconds();
+  let time, session;
 
-switch(true){
-    case (hours < 10):
-        hours = "0" + hours;
-    case (minutes < 10):
-        minutes = "0" + minutes;
-    case (seconds < 10):
-        seconds = "0" + seconds;
-    case (hours > 12):
-        hours -= 12;   
+  switch (true) {
+    case hours < 10:
+      hours = "0" + hours;
+      break;
+    case minutes < 10:
+      minutes = "0" + minutes;
+      break;
+    case seconds < 10:
+      seconds = "0" + seconds;
+      break;
+  }
+  if(hours > 12){
+    hours = "0" + (hours - 12)
+  }
+  if (hours <= 12) {
+    session = "PM";
+  } else {
+    session = "AM";
+  }
+
+  time = `${hours}:${minutes}:${seconds} ${session}`;
+  (display.textContent = time), setTimeout(timeLoad, 1000);
 }
-    if(hours <= 12){
-        session = "PM"
-    } else {
-        session = "AM"
-    }
 
-
- time = `${hours}:${minutes}:${seconds} ${session}`;
-display.textContent = time
-, setTimeout(timeLoad, 1000)}
-
-timeLoad()
+timeLoad();
